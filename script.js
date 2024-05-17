9972902c9585b58d19ebbe1ee7e4d05f9f5e6a44
Salman Sheikh <salmansheikhutk@gmail.com>
2024-05-17 08:39:01 -0400

    First 4 functions complete for add, subtract, multiply and divide

---
+++ script.js
@@ -0,0 +1,19 @@
+// Add
+function add(a,b) {
+    return a + b
+}
+
+// Subtract
+function subtract(a,b) {
+    return a - b
+}
+
+// Multiply
+function multiply(a,b){
+    return a * b
+}
+
+// Divide
+function divide(a,b){
+    return a/b
+}
