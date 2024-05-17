9bf1c6598e0c8cf0863a2b18c009463afbb9a65d
Salman Sheikh <salmansheikhutk@gmail.com>
2024-05-17 16:09:48 -0400

    color complete for front end buttons

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 // Create three variables
-// let firstnumber = 0
-// let secondnumber = 0
-// let operator = 0
+let firstnumber = 0
+let secondnumber = 0
+let operator = 0
+let display_value = 0
+
+// Display Values
+function display(firstnumber,operator,secondnumber){
+    display_value =  operate(firstnumber,operator,secondnumber)
+    return display_value
+}
 
 // Operate
-function operate(firstnumber,secondnumber,operator){
+function operate(firstnumber,operator,secondnumber){
     if (operator == '+') {
         return add(firstnumber,secondnumber)
     } else if (operator == '-') {
@@ -36,7 +43,36 @@ function divide(a,b){
     return a/b
 }
 
-console.log(operate(4,2,'+'))
-console.log(operate(4,2,'-'))
-console.log(operate(4,2,'*'))
-console.log(operate(4,2,'/'))
+console.log(display(1,"+",2)) //check if calc works
+
+// Hover over and change color
+
+function mouse() {
+    const btns = document.querySelectorAll(".hov");
+    btns.forEach((btn) => {
+        btn.addEventListener("mouseover", changecolor)
+        btn.addEventListener("mouseout", resetcolor)
+    })
+}
+
+// function for mouseover color change
+function changecolor () {
+    this.style.backgroundColor = "lightgrey"
+    this.style.color = "black"
+}
+
+// reset color
+function resetcolor () {
+    this.style.backgroundColor = "";
+    this.style.color = ""
+}
+
+mouse();
+// store 4 clicks: first number, operator, second number, =
+// Store first number clicked
+
+// Store operator clicked
+
+// Store second number clicked
+
+// Store = sign clicked, which shows result in display
