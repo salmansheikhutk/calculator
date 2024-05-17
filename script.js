6315b093ccd6d35da7290ebf6d7f8dd9b58580fe
Salman Sheikh <salmansheikhutk@gmail.com>
2024-05-17 08:52:27 -0400

    operate function is working

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 // Create three variables
-let firstnumber = 0
-let secondnumber = 0
-let operator = 0
+// let firstnumber = 0
+// let secondnumber = 0
+// let operator = 0
 
-
+// Operate
+function operate(firstnumber,secondnumber,operator){
+    if (operator == '+') {
+        return add(firstnumber,secondnumber)
+    } else if (operator == '-') {
+        return subtract(firstnumber,secondnumber)
+    } else if (operator == '*') {
+        return multiply(firstnumber,secondnumber)
+    } else if (operator == '/') {
+        return divide(firstnumber,secondnumber)
+    }
+}
 
 // Add
 function add(a,b) {
@@ -25,3 +36,7 @@ function divide(a,b){
     return a/b
 }
 
+console.log(operate(4,2,'+'))
+console.log(operate(4,2,'-'))
+console.log(operate(4,2,'*'))
+console.log(operate(4,2,'/'))
